3bb0e02a55e766f0d40cd893d02a56f5f2b0c8d7
You Song Hou <yousonghou@yahoo.com>
2020-11-09 01:36:50 -0500

    add a query to get all topics from quickbase

--- src/components/Topics.vue
+++ src/components/Topics.vue
@@ -5,8 +5,25 @@
 </template>
 
 <script>
+import { headers } from '../config';
+
 export default {
-  name: 'Topics'
+  name: 'Topics',
+  mounted(){
+    fetch('https://api.quickbase.com/v1/records/query',
+      {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({"from":"bqyx3rp8i","select":[5,6,7,8,9]})
+      })
+    .then(res => {
+      if(res.ok){
+        return res.json().then(res => console.log(res));
+      }
+      return res.json().then(resBody => Promise.reject({status: res.status, ...resBody}));
+    })
+    .catch(err => console.log(err))
+  }
 }
 </script>
 
