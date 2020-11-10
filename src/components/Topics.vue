b3d179bd607b0c65d5f83094c979bac15ca49003
You Song Hou <yousonghou@yahoo.com>
2020-11-09 23:40:01 -0500

    display the list of topics

--- src/components/Topics.vue
+++ src/components/Topics.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>Topics</h1>
+    <div v-bind:key="topic[3].value" v-for="topic in topics">
+      <h2>{{ topic[6].value }}</h2>
+      <p>{{ topic[7].value }}</p>
+      <p>Likes {{ topic[8].value }} -- {{ topic[14].value }}</p>
+      <p>{{ topic[1].value }}</p>
+    </div>
   </div>
 </template>
 
@@ -9,16 +15,21 @@ import { headers } from '../config';
 
 export default {
   name: 'Topics',
+  data: () => ({
+    topics: []
+  }),
   mounted(){
     fetch('https://api.quickbase.com/v1/records/query',
       {
         method: 'POST',
         headers: headers,
-        body: JSON.stringify({"from":"bqyx3rp8i","select":[5,6,7,8,9]})
+        body: JSON.stringify({"from":"bqyx3rp8i","select":[1,3,6,7,8,14]})
       })
     .then(res => {
       if(res.ok){
-        return res.json().then(res => console.log(res));
+        return res.json().then(res => {
+          this.topics = res.data;
+        });
       }
       return res.json().then(resBody => Promise.reject({status: res.status, ...resBody}));
     })
