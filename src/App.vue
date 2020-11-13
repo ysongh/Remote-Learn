81929588e029b17600ef16f84fe04103ea7d2de5
You Song Hou <yousonghou@yahoo.com>
2020-11-12 23:18:47 -0500

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/layout/Navbar';
 
 export default {
   name: 'App',
   components: {
-    
+    Navbar
   }
 }
 </script>
