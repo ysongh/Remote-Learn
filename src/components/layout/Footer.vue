a8a089165763ebdb354c8615a6c28f33b4185657
You Song Hou <yousonghou@yahoo.com>
2020-11-16 00:49:08 -0500

    add footer

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+   <footer class="primary-color p-4 mt-5 text-center">
+        Copyright &copy; 2020 Remote Learn
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
