6ae38e4a4459fe62ce544865d730cb4ca95d7780
You Song Hou <yousonghou@yahoo.com>
2021-01-26 23:57:17 -0500

    call an api to deposit tokens into a channel on Raiden Network

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -4,6 +4,9 @@
         <router-link v-if="!channel" class="btn primary-color" :to="{ path: '/addchannel/'+ this.$route.params.email}">
           Create Channel
         </router-link>
+        <button v-else class="btn primary-color" @click="depositToken(10)">
+          Give 10 Teach Token
+        </button>
     </div>
 </template>
 
@@ -19,6 +22,20 @@ export default {
     const userId = await getUserIdByEmailAPI(this.$route.params.email);
     const channelData = await getChannelAddressAPI(userId);
     this.channel = channelData;
+    console.log(this.channel)
+  },
+  methods:{
+    async depositToken(amount){
+      const res = await fetch('http://localhost:5001/api/v1/channels/0x8AA23bF3065edF4e8748FE9cCb76CD41BA75574E/' + this.channel, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "total_deposit": amount
+        })
+      });
+      const data = await res.json();
+      console.log(data)
+    }
   }
 }
 </script>
