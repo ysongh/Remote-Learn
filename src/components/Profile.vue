1609b9eb1f3fa4378d26c9f9edd2cc561d9b9b68
You Song Hou <yousonghou@yahoo.com>
2021-01-27 00:49:39 -0500

    call an api to get channel data from Raiden Network

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -4,9 +4,12 @@
         <router-link v-if="!channel" class="btn primary-color" :to="{ path: '/addchannel/'+ this.$route.params.email}">
           Create Channel
         </router-link>
-        <button v-else class="btn primary-color" @click="depositToken(10)">
-          Give 10 Teach Token
-        </button>
+        <div v-else>
+          <p>Balance: {{balance}}</p>
+           <button  class="btn primary-color" @click="depositToken(10)">
+            Give 10 Teach Token
+          </button>
+        </div>
     </div>
 </template>
 
@@ -17,12 +20,16 @@ export default {
   name: 'Profile',
   data: () => ({
     channel: null,
+    balance: 0
   }),
   async mounted(){
     const userId = await getUserIdByEmailAPI(this.$route.params.email);
     const channelData = await getChannelAddressAPI(userId);
     this.channel = channelData;
-    console.log(this.channel)
+
+    const res = await fetch('http://localhost:5001/api/v1/channels/0x8AA23bF3065edF4e8748FE9cCb76CD41BA75574E/' + this.channel);
+    const data = await res.json();
+    this.balance = data.balance;
   },
   methods:{
     async depositToken(amount){
@@ -34,7 +41,7 @@ export default {
         })
       });
       const data = await res.json();
-      console.log(data)
+      this.balance = data.balance;
     }
   }
 }
