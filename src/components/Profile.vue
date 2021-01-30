968fb364ea77457c18bbce756296281aa0276f1a
You Song Hou <yousonghou@yahoo.com>
2021-01-29 23:45:59 -0500

    add loading on the profile page

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -18,13 +18,14 @@
               <div v-if="this.email && this.email !== this.$route.params.email && channel">
                 <h2 class="h3">Send Teach Token</h2>
                 
-                <div class="d-flex flex-wrap">
+                <div v-if="!loading" class="d-flex flex-wrap">
                   <div v-for="n in numbers" :key="n">
                     <button class="btn primary-color mr-3 mb-3" @click="depositToken(n)">
                       Give {{n}}
                     </button>
                   </div>
                 </div>
+                <Spinner v-else />
               </div>
 
               <div v-if="this.email === this.$route.params.email && channel">
@@ -36,7 +37,8 @@
                     name="Withdraw Amount"
                     v-model="withdrawAmount">
                 </div>
-                <button class="btn primary-color" @click="withdrawToken()">Withdraw</button>
+                <button v-if="!loading" class="btn primary-color" @click="withdrawToken()">Withdraw</button>
+                <Spinner v-else />
               </div>
             </div>
         </div>
@@ -47,14 +49,18 @@
 import { mapGetters } from 'vuex';
 import { getUserIdByEmailAPI, getChannelAddressAPI } from '../api/users';
 
+import Spinner from './common/Spinner';
+
 export default {
+  components: { Spinner },
   name: 'Profile',
   data: () => ({
     numbers: [1, 5, 10, 25, 50, 100, 250, 500, 1000],
     channel: null,
     balance: 0,
     channelData: null,
-    withdrawAmount: 0
+    withdrawAmount: 0,
+    loading: false
   }),
   async mounted(){
     const userId = await getUserIdByEmailAPI(this.$route.params.email);
@@ -70,6 +76,8 @@ export default {
   computed: mapGetters(['email']),
   methods:{
     async depositToken(amount){
+      this.loading = true;
+
       const res = await fetch('http://localhost:5001/api/v1/channels/0x8AA23bF3065edF4e8748FE9cCb76CD41BA75574E/' + this.channel, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
@@ -79,8 +87,11 @@ export default {
       });
       const data = await res.json();
       this.balance = data.balance;
+      this.loading = false;
     },
     async withdrawToken(){
+      this.loading = true;
+
       const res = await fetch('http://localhost:5001/api/v1/channels/0x8AA23bF3065edF4e8748FE9cCb76CD41BA75574E/' + this.channel, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
@@ -92,6 +103,7 @@ export default {
       console.log(data);
       this.balance -= this.withdrawAmount;
       this.withdrawAmount = 0;
+      this.loading = false;
     }
   }
 }
